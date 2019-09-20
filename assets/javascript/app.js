6be987fa27bb842bfbf8c88126a2a28aaaaae37e
Haakasaurus <kramerhjohnson@gmail.com>
2019-09-20 09:42:29 -0700

    Added anime.js animations to start of page, defined functions to pick and display a random question and answers.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,20 +28,84 @@ let gameQuestions = {
         question: "What would you use a cam for?",
         answer: "Protecting yourself from a fall while rock climbing.",
         wrongAnswers: ["To tighten your ski boots.", "To ascend a rope.", "As a guy to toss you a brewski."]
+    },
+
+
+
+    
+
+
+}
+
+function randomQuesNumber () {
+    return Math.floor((Math.random() * 6) + 1);
+}
+function chooseRanAnsSelctor () {
+    return $("#answer-" + Math.floor((Math.random() * 4) + 1));
+}
+
+function questionTimer () {
+    let timer = setTimeout(function (){/*TODO choose what happens when you run of out time.*/}, 30000);
+}
+
+function getRandomQuestion (randomNum) {
+    return gameQuestions["question" + randomNum];
+}
+
+function pickAndDisplayQuestion () {
+    // Assign a random question object.
+    let currentQuestion = getRandomQuestion(randomQuesNumber());
+
+    // Get the selector of a random list item to display the answer.
+    let rightAnswerSelctor = chooseRanAnsSelctor();
+
+    // Display the current question.
+    $("#question").text(currentQuestion.question);
+
+    // Display the correct answer.
+    rightAnswerSelctor.text(currentQuestion.answer);
+
+    // Loop through the wrong answers array.
+    for (i = 0; i < currentQuestion.wrongAnswers.length; i++) {
+        // Assign random answer selector to the currentWrongAnswer.
+        let currentWrongAnswer = chooseRanAnsSelctor();
+        /* Continue to reassign the random selector until it chooses an empty
+            answer div. */
+        while (!currentWrongAnswer.is(':empty')) {
+            currentWrongAnswer = chooseRanAnsSelctor();
+        }
+        currentWrongAnswer.text(currentQuestion.wrongAnswers[i]);
     }
 }
 
-const anime = require('anime');
 
-$(document).ready(function() {
+anime({
+    targets: '.main-container',
+    translateY: {
+        value: -940,
+        duration: 2000,
+        endDelay: 1000
+    },
+});
 
-    anime({
-        targets: 'div',
-        translateX: 250,
-        rotate: '1turn',
-        backgroundColor: '#FFF',
-        duration: 800
-      });
-    // $("#start-button").show();
+anime({
+    targets: 'h1#header',
+    translateX: -3000,
+    translateY: -1000,
+    duration: 2000,
+    delay: 500
+});
+
+$(document).ready(function () {
+    $("#start-button").show();
+    $("li").hide();
+})
+
+
+$("#start-button").on('click', function() {
+    $("#start-button").hide();
+    $("li").show();
+    pickAndDisplayQuestion();
+
 
 });
